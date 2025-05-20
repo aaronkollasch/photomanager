b9dee16322c384280b90b30bf52452ef4b74eb5c
Aaron Kollasch <aaron@kollasch.dev>
2025-05-20 01:38:51 -0400

    Add test for combined async queue timeout

--- tests/unit_tests/test_async_base.py
+++ tests/unit_tests/test_async_base.py
@@ -1,6 +1,12 @@
 import logging
+import sys
 from asyncio import run, sleep
 from dataclasses import dataclass
+
+if sys.version_info >= (3, 11):
+    from asyncio import timeout
+else:
+    from async_timeout import timeout
 
 import pytest
 
@@ -101,6 +107,17 @@ def test_async_execute_queue():
     worker = AsyncTimeoutWorker(num_workers=2, job_timeout=0.0002)
     all_jobs = [TimeoutJob(t) for t in (0.0001, 0.00005, 0.00015)]
     assert run(worker.execute_queue(all_jobs)) == {}
+
+
+def test_async_execute_queue_multiple_workers():
+    worker = AsyncTimeoutWorker(num_workers=8, job_timeout=0.002)
+    all_jobs = [TimeoutJob(0.001) for _ in range(8)]
+
+    async def run_timeout():
+        async with timeout(0.002):
+            return await worker.execute_queue(all_jobs)
+
+    assert run(run_timeout()) == {}
 
 
 def test_async_execute_queue_timeout_error(caplog):
